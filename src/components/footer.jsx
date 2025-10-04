002e8fee7df13f6e48d2cfd34029c23a9195ebbe
David495 <davidnwaogu961@gmail.com>
2025-10-04 12:42:21 +0100

    Fixed the footer layout

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,37 +5,49 @@ import Footerstyle from './footer.module.css'
 const Footer = () => {
   return (
     <>
-    <footer className={Footerstyle.footer_section}>
-        <div className="footer_logo">
-            <Link to='/' className={Footerstyle.footer_logo}>Learnly</Link>
-            <p>Learn smarter, create faster, achieve more.</p>
-        </div>
+    <footer className={Footerstyle.footer}>
+        <div className={Footerstyle.inner}>
+          <div className={Footerstyle.brand}>
+            <Link to='/' className={Footerstyle.brand_name}>Learnly</Link>
+            <p className={Footerstyle.brand_tagline}>Learn smarter, create faster, achieve more.</p>
+          </div>
 
-        <div className={Footerstyle.footer_links}>  
-            <Link to='/about' className={Footerstyle.footer_links}>About</Link>
-            <Link to='/contact' className={Footerstyle.footer_links}>Contact</Link>
-            <Link to='/privacy' className={Footerstyle.footer_links}>Privacy</Link>
-            <Link to='/terms' className={Footerstyle.footer_links}>Terms</Link>
-        </div>
+          <nav className={Footerstyle.column} aria-label="Company">
+            <h3 className={Footerstyle.column_title}>Company</h3>
+            <ul className={Footerstyle.link_list}>
+              <li><Link to='/about' className={Footerstyle.link}>About</Link></li>
+              <li><Link to='/contact' className={Footerstyle.link}>Contact</Link></li>
+              <li><Link to='/privacy' className={Footerstyle.link}>Privacy</Link></li>
+              <li><Link to='/terms' className={Footerstyle.link}>Terms</Link></li>
+            </ul>
+          </nav>
 
-        <div className={Footerstyle.footer_social}>
-            <Link to='/facebook' className={Footerstyle.footer_social}>Facebook</Link>
-            <Link to='/twitter' className={Footerstyle.footer_social}>Twitter</Link>
-            <Link to='/instagram' className={Footerstyle.footer_social}>Instagram</Link>
-            <Link to='/linkedin' className={Footerstyle.footer_social}>LinkedIn</Link>
-        </div>
-        <div className={Footerstyle.footer_contact}>
-        <Link to='/facebook' className={Footerstyle.footer_contact}>Support</Link>
-            <Link to='/help-center' className={Footerstyle.footer_contact}>Help Center</Link>
-            <Link to='/faqs' className={Footerstyle.footer_contact}>FAQs</Link>
-            <Link to='/terms' className={Footerstyle.footer_contact}>Terms of Service</Link>
-            <Link to='/privacy' className={Footerstyle.footer_contact}>Privacy Policy</Link>
-        </div>
-    </footer>
+          <nav className={Footerstyle.column} aria-label="Social">
+            <h3 className={Footerstyle.column_title}>Social</h3>
+            <ul className={Footerstyle.link_list}>
+              <li><Link to='/facebook' className={Footerstyle.link}>Facebook</Link></li>
+              <li><Link to='/twitter' className={Footerstyle.link}>Twitter</Link></li>
+              <li><Link to='/instagram' className={Footerstyle.link}>Instagram</Link></li>
+              <li><Link to='/linkedin' className={Footerstyle.link}>LinkedIn</Link></li>
+            </ul>
+          </nav>
 
-    <div className={Footerstyle.footer_copyright}>
-            <p>© 2025 Learnly. All rights reserved.</p>
+          <nav className={Footerstyle.column} aria-label="Support">
+            <h3 className={Footerstyle.column_title}>Support</h3>
+            <ul className={Footerstyle.link_list}>
+              <li><Link to='/support' className={Footerstyle.link}>Support</Link></li>
+              <li><Link to='/help-center' className={Footerstyle.link}>Help Center</Link></li>
+              <li><Link to='/faqs' className={Footerstyle.link}>FAQs</Link></li>
+              <li><Link to='/terms' className={Footerstyle.link}>Terms of Service</Link></li>
+              <li><Link to='/privacy' className={Footerstyle.link}>Privacy Policy</Link></li>
+            </ul>
+          </nav>
         </div>
+      </footer>
+
+      <div className={Footerstyle.copyright}>
+        <p>© 2025 Learnly. All rights reserved.</p>
+      </div>
     </>
     
   )
